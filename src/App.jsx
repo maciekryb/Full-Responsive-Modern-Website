a4a58b05e1f1a948fffbedce16f31cc5f77732c1
Maciej Rybak <maciek.rybak95@gmail.com>
2023-02-04 20:28:16 +0100

    Add basic css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Brand, CTA, Navbar } from "./components";
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import "./App.css";
 
 const App = () => {
   return (
