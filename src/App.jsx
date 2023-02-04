8003bea7f383eedc331bd90b3053ca37e5863179
Maciej Rybak <maciek.rybak95@gmail.com>
2023-02-04 18:40:21 +0100

    Add container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 
-import { Article, Brand, CTA } from "./components";
+import { Brand, CTA, Navbar } from "./components";
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
 
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 };
