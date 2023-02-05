9b21334a808cd17ff8b74bf83611dff4e736c01c
Maciej Rybak <maciek.rybak95@gmail.com>
2023-02-05 22:30:16 +0100

    Add header section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
 
 const brand = () => {
   return (
-    <div>
-      <h1>Brand</h1>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google"></img>
+      </div>
+      <div>
+        <img src={slack} alt="google"></img>
+      </div>
+      <div>
+        <img src={atlassian} alt="google"></img>
+      </div>
+      <div>
+        <img src={dropbox} alt="google"></img>
+      </div>
+      <div>
+        <img src={shopify} alt="google"></img>
+      </div>
     </div>
   );
 };
