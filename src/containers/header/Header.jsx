6fefd8ad7230ad26381abd9d4d446c0a14f2ea38
Maciej Rybak <maciek.rybak95@gmail.com>
2023-02-05 13:25:16 +0100

    left section off header added

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -19,7 +19,11 @@ const Header = () => {
         </div>
         <div className="gpt3__header-content__people">
           <img src={people} />
+          <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
+        {/* <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div> */}
       </div>
     </div>
   );
