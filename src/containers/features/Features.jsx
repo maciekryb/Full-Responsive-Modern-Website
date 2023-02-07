28aafdcef19dda627958e009a18a2c8a5ee0f2cc
Maciej Rybak <maciek.rybak95@gmail.com>
2023-02-07 22:28:02 +0100

    add css for feature page

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -23,15 +23,16 @@ const featuresData = [
 
 const Features = () => {
   return (
-    <div className="gpt3__features section__padding" id="feautres">
-      <div className="gpt3__features-heading"></div>
-      <h1 className="gradient__text">
-        The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
-      </h1>
-      <p>Request Early Access to Get Started</p>
-      <div className="gpt3__feature-container">
+    <div className="gpt3__features section__padding" id="feature">
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
         {featuresData.map((item, index) => (
-          <Feature title={item.title} text={item.text} ket={item.title + index} />
+          <Feature title={item.title} text={item.text} key={item.title + index} />
         ))}
       </div>
     </div>
